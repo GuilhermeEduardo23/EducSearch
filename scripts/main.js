7e5ce8b35ed2e34a4edbc09f9103bcc75b745f1e
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-03-31 10:19:02 -0300

    Inclusão da API do ChatGPT

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,23 @@ spanDarkMode.addEventListener(`click`, () => {
     else
         spanDarkMode.innerHTML = `<ion-icon name="moon"></ion-icon>`;
 });
+
+/*----------------------------------------------------------------*/
+
+//Inclusão da Api do ChatGPT
+
+//Acessar o campo de texto
+const mainSearch = document.querySelector(`.search`);
+
+if(mainSearch) {
+    //Aguardar o usuário clicar no botão Enviar
+    mainSearch.addEventListener(`load`, async (e) => {
+        //Bloquear o recarregamento da página
+        e.preventDefault();
+
+        //Receber o valor do input
+        let pergunta = document.querySelector(`input`).value;
+
+        console.log(pergunta);
+    });
+}
